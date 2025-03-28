51a9c6288b3e8b30fa741f70d8c7639a5ffb8a64
lvth4m <realsuzucry@gmail.com>
2025-03-28 23:18:03 +0300

    Update Hero.svelte

--- src/components/organisms/Hero.svelte
+++ src/components/organisms/Hero.svelte
@@ -12,8 +12,8 @@
 
 <section id="home" class="wrapper">
 	<div>
-		<h1>afn</h1>
-		<h4>Hobbyist illustrator and designer. <br />Attempting to program.</h4>
+		<h1>Gökhan Ş.</h1>
+		<h4>Nothing much about me now. <br />We'll see it later.</h4>
 		<div class="socials">
 			<Socials />
 		</div>
