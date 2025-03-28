b8618f8ccb267d28c0c89bcdd82ad1876e0cdfaa
lvth4m <realsuzucry@gmail.com>
2025-03-28 23:16:49 +0300

    Update About.svelte

--- src/components/organisms/About.svelte
+++ src/components/organisms/About.svelte
@@ -4,7 +4,7 @@
 
 	// i didnt write this idk
 	let getAge = () => {
-		let birthDate = new Date('2007/03/24');
+		let birthDate = new Date('2000/01/01');
 		const ageMs = Date.now() - birthDate.getTime();
 		const preciseAge = (ageMs / 31536000000).toFixed(10);
 		return preciseAge;
@@ -23,19 +23,19 @@
 	<div class="text">
 		<h2>bio</h2>
 		<p>
-			Hey there, I'm afn! :] I'm a <Tooltip tip={age}
+			Hey there, I'm Gökhan. I'm a <Tooltip tip={age}
 				><span>{Math.floor(Number(age))}</span></Tooltip
 			>
-			year old digital artist and graphic/UI designer based in Canada. I’ve taken art seriously since
-			<span>2017</span>, and have been doodling silly anime characters since <span>2020</span>.
-			Recently, however, I’ve grown a knack for programming. I like contributing to
+			year old student and amateur guitarist based in Turkey. I’ve loved music since
+			<span>my childhood</span>, and have been playing the electric guitar since <span>2024</span>.
+			Recently, however, I’ve been fond of thrash metal. I like listening and playing
 			<Tooltip tip="🤓">
 				<a href="https://github.com/xafn" target="_blank" rel="noreferrer">
-					<span>open source</span>
+					<span>old-school thrash and crossover thrash</span>
 				</a>
 			</Tooltip>
-			as a web developer, which is probably the reason why you've ended up here. Currently struggling
-			with Svelte and Typescript.
+			whenever I possibly can, which is probably the reason why my brain is rotten. Currently struggling
+			with writing songs and producing my own short demo album. I will link my music to this website when they're done.
 		</p>
 	</div>
 </section>
