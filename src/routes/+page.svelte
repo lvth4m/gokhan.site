2419f8297a2ce8d20b66c59f8fa817b051cd03b1
Gökhan Ş. <realsuzucry@gmail.com>
2025-05-30 20:04:46 +0300

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,6 @@
 	import NavHost from '../components/molecules/NavHost.svelte';
 	import Hero from '../components/organisms/Hero.svelte';
 	import About from '../components/organisms/About.svelte';
-	import Repos from '../components/organisms/Repos.svelte';
 	import Footer from '../components/molecules/Footer.svelte';
 </script>
 
@@ -10,6 +9,5 @@
 <main>
 	<Hero />
 	<About />
-	<Repos />
 	<Footer />
 </main>
